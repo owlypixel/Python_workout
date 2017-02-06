a753e681ce51bf9447d769c5ea21fdbcd86ce62b
Owlypixel <owlypixel@gmail.com>
2017-02-06 22:18:17 +0200

    reading excel spreadsheets

---
+++ EXCEL_workout/read_xls.py
@@ -0,0 +1,21 @@
+﻿#! python3
+# -*- coding: utf-8 -*-
+
+#pip install openpyxl
+# reading excel spreadsheets
+import sys
+import os
+import openpyxl
+
+workbook = openpyxl.load_workbook('Channels.xlsx')
+print(type(workbook))
+sheet = workbook.get_sheet_by_name('EN')
+print(type(sheet))
+sheetnames = workbook.get_sheet_names()
+print(sheetnames)
+cell = sheet['A1']
+print(str(cell.value))
+print(str(sheet['B2'].value))
+
+for i in range(1, 10):
+	print(i, sheet.cell(row=i, column=1).value)
