8965cb3ea15601cca6eca8f654c0d28e65699b84
Owlypixel <owlypixel@gmail.com>
2017-05-08 23:04:40 +0300

    Add products in categories, images

--- Django/electronic_kit/products/views.py
+++ Django/electronic_kit/products/views.py
@@ -4,7 +4,6 @@ from django.views.generic import View, DetailView
 from django.db.models import Count
 # from django.http import HttpResponse
 
-from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
